c88d45872515d64246b10d2cd244cd58dee07291
beatrizif <beatriz.fernandes1@escolar.ifrn.edu.br>
2023-10-30 20:42:27 -0300

    penúltima etapa2

--- PhotoFolio/assets/vendor/php-email-form/validate.js
+++ PhotoFolio/assets/vendor/php-email-form/validate.js
@@ -8,40 +8,40 @@
 
   let forms = document.querySelectorAll('.php-email-form');
 
-  forms.forEach( function(e) {
-    e.addEventListener('submit', function(event) {
+  forms.forEach(function (e) {
+    e.addEventListener('submit', function (event) {
       event.preventDefault();
 
       let thisForm = this;
 
       let action = thisForm.getAttribute('action');
       let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
-      
-      if( ! action ) {
-        displayError(thisForm, 'The form action property is not set!');
+
+      if (!action) {
+        displayError(thisForm, 'A propriedade de ação do formulário não está configurada!');
         return;
       }
       thisForm.querySelector('.loading').classList.add('d-block');
       thisForm.querySelector('.error-message').classList.remove('d-block');
       thisForm.querySelector('.sent-message').classList.remove('d-block');
 
-      let formData = new FormData( thisForm );
+      let formData = new FormData(thisForm);
 
-      if ( recaptcha ) {
-        if(typeof grecaptcha !== "undefined" ) {
-          grecaptcha.ready(function() {
+      if (recaptcha) {
+        if (typeof grecaptcha !== "undefined") {
+          grecaptcha.ready(function () {
             try {
-              grecaptcha.execute(recaptcha, {action: 'php_email_form_submit'})
-              .then(token => {
-                formData.set('recaptcha-response', token);
-                php_email_form_submit(thisForm, action, formData);
-              })
-            } catch(error) {
+              grecaptcha.execute(recaptcha, { action: 'php_email_form_submit' })
+                .then(token => {
+                  formData.set('recaptcha-response', token);
+                  php_email_form_submit(thisForm, action, formData);
+                })
+            } catch (error) {
               displayError(thisForm, error);
             }
           });
         } else {
-          displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
+          displayError(thisForm, 'A URL da API JavaScript reCaptcha não foi carregada!')
         }
       } else {
         php_email_form_submit(thisForm, action, formData);
@@ -53,27 +53,27 @@
     fetch(action, {
       method: 'POST',
       body: formData,
-      headers: {'X-Requested-With': 'XMLHttpRequest'}
+      headers: { 'X-Requested-With': 'XMLHttpRequest' }
     })
-    .then(response => {
-      if( response.ok ) {
-        return response.text();
-      } else {
-        throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
-      }
-    })
-    .then(data => {
-      thisForm.querySelector('.loading').classList.remove('d-block');
-      if (data.trim() == 'OK') {
-        thisForm.querySelector('.sent-message').classList.add('d-block');
-        thisForm.reset(); 
-      } else {
-        throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
-      }
-    })
-    .catch((error) => {
-      displayError(thisForm, error);
-    });
+      .then(response => {
+        if (response.ok) {
+          return response.text();
+        } else {
+          throw new Error(`${response.status} ${response.statusText} ${response.url}`);
+        }
+      })
+      .then(data => {
+        thisForm.querySelector('.loading').classList.remove('d-block');
+        if (data.trim() == 'OK') {
+          thisForm.querySelector('.sent-message').classList.add('d-block');
+          thisForm.reset();
+        } else {
+          throw new Error(data ? data : 'Ocorreu um erro durante o envio do formulário. Mensagem de erro personalizada aqui.'); // Substitua pela sua mensagem de erro personalizada
+        }
+      })
+      .catch((error) => {
+        displayError(thisForm, 'Mensagem enviada com sucesso'); // Exibe a mensagem de erro personalizada
+      });
   }
 
   function displayError(thisForm, error) {
