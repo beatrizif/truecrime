b539b8a51fcb1009960dcc4cfb5eb2539beeb889
MARIA DE JESUS DOS SANTOS MEDEIROS <jesus.maria@academico.ifrn.edu.br>
2023-11-10 19:47:02 -0300

    bd finish

--- PhotoFolio/forms/contact.php
+++ PhotoFolio/forms/contact.php
@@ -4,14 +4,18 @@ $P2 = $_POST['P2'];
 $P3 = $_POST['P3'];
 $P4 = $_POST['P4'];
 $P5 = $_POST['P5'];
+$P6 = $_POST['P6'];
 
 $conn = new PDO('mysql:host=127.0.0.1:3306;dbname=db_truecrime', 'root', 'mjsm2004');
 
 try{ 
-$stmt = $conn->prepare("INSERT INTO tb_avaliacoes (P1, P2, P3,P4,P5)
- VALUES ('".$P1."', '".$P2."', '".$P3."', '".$P4."', '".$P5."' );");
+$stmt = $conn->prepare("INSERT INTO tb_avaliacoes (P1, P2, P3,P4,P5,P6)
+ VALUES ('".$P1."', '".$P2."', '".$P3."', '".$P4."', '".$P5."', '".$P6."' );");
 $stmt->execute();
-echo "<center><h1> Registro de bioma concluído </h1></center>  <a href='/'><button class='btn btn-info'>Home</button></a>";
+echo "<center><script>
+alert('Formulário enviado com sucesso!');
+window.location.href = '../contact.html';
+</script>;</center>";
 } catch(PDOException $e) {
     echo "<br>" . $e->getMessage();
   }
