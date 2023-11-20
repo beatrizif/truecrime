662b87609bc7e1cf46e11536683dd7a7f59f3041
MariaJesus18 <mariadejesusmdrs@gmail.com>
2023-11-20 19:57:59 -0300

    bd sqlite3

--- PhotoFolio/forms/contact.php
+++ PhotoFolio/forms/contact.php
@@ -1,3 +1,5 @@
+
+
 <?php
 $P1 = $_POST['P1'];
 $P2 = $_POST['P2'];
@@ -6,18 +8,30 @@ $P4 = $_POST['P4'];
 $P5 = $_POST['P5'];
 $P6 = $_POST['P6'];
 
-$conn = new PDO('mysql:host=127.0.0.1:3306;dbname=db_truecrime', 'root', 'mjsm2004');
+try {
+    $conn = new PDO('sqlite:database.sqlite');
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-try{ 
-$stmt = $conn->prepare("INSERT INTO tb_avaliacoes (P1, P2, P3,P4,P5,P6)
- VALUES ('".$P1."', '".$P2."', '".$P3."', '".$P4."', '".$P5."', '".$P6."' );");
-$stmt->execute();
-echo "<center><script>
+    // Corrigindo a tabela
+    $conn->exec("CREATE TABLE IF NOT EXISTS tb_avaliacoes (
+        id INTEGER PRIMARY KEY,
+        P1 VARCHAR(45) NOT NULL,
+        P2 VARCHAR(45) NOT NULL,
+        P3 VARCHAR(45) NOT NULL,
+        P4 VARCHAR(45) NOT NULL,
+        P5 VARCHAR(45) NOT NULL,
+        P6 VARCHAR(45) NOT NULL
+    )");
+
+    // Usando instrução preparada para evitar SQL injection
+    $stmt = $conn->prepare("INSERT INTO tb_avaliacoes (P1, P2, P3, P4, P5, P6) VALUES (?, ?, ?, ?, ?, ?)");
+    $stmt->execute([$P1, $P2, $P3, $P4, $P5, $P6]);
+
+    echo "<center><script>
 alert('Formulário enviado com sucesso!');
 window.location.href = '../contact.html';
 </script>;</center>";
-} catch(PDOException $e) {
-    echo "<br>" . $e->getMessage();
-  }
-
+} catch (PDOException $e) {
+    echo "Erro: " . $e->getMessage();
+}
 ?>
